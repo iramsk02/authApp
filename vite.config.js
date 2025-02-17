01b49ac8dfb1bc4d091a99cbf3ec4eb100389dcd
Iram Saba Khan <161953870+iramsk02@users.noreply.github.com>
2025-02-17 09:33:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/authApp/",  // Ensure this matches your repo name
+  base: "/authApp",  // Ensure this matches your repo name
   build: {
     outDir: "dist",
   }
